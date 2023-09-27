1f889f808327ce3934429686ce46be7cb83683ba
Devvaru <syeraru@gmail.com>
2023-09-26 22:34:43 -0600

    Create back function and handle state history

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(newMode) {
-    setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
   }
 
-  return { mode, transition };
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+  };
+
+  return { mode: history[history.length - 1], transition, back };
 }
 
 // As seen here, the `useVisualMode` function can take an initial argument to set the mode state. We then return an object `{ mode }`, which can also be written as `{ mode: mode }`. This lets our tests (and components) access the current value of the mode from the hook.
