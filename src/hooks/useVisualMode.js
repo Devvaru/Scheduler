dc8050fc116123ca09ebddf563dd1be448a1969b
Devvaru <syeraru@gmail.com>
2023-09-26 22:55:05 -0600

    Implement limit on back and transition functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,15 +3,21 @@ import { useState } from 'react';
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setHistory(prev => [...prev, newMode]);
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode])
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
   }
 
   function back() {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    }
   };
 
   return { mode: history[history.length - 1], transition, back };
-}
+};
 
 // As seen here, the `useVisualMode` function can take an initial argument to set the mode state. We then return an object `{ mode }`, which can also be written as `{ mode: mode }`. This lets our tests (and components) access the current value of the mode from the hook.
